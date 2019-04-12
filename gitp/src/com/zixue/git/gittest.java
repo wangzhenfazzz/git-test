976cd4e33c07f0e14dd4bc274cd970377995967b
wangzi <wangzi.ziyou.com>
2019-04-12 16:43:52 +0800

    push到远程仓库就

--- gitp/src/com/zixue/git/gittest.java
+++ gitp/src/com/zixue/git/gittest.java
@@ -3,5 +3,6 @@ package com.zixue.git;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("niha二手房东莞o git");
+        System.out.println("niha二手房东莞o git");System.out.println("niha二手房东莞o git");System.out.println("niha二手房东莞o git");System.out.println("niha二手房东莞o git");
     }
 }
