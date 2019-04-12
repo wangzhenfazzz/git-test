b14db9d8446436df5dd0019c5f53710365087978
wangzi <wangzi.ziyou.com>
2019-04-12 16:35:23 +0800

    2次提交git'到本地仓库

--- gitp/src/com/zixue/git/gittest.java
+++ gitp/src/com/zixue/git/gittest.java
@@ -2,6 +2,6 @@ package com.zixue.git;
 
 public class gittest {
     public static void main(String[] args) {
-        System.out.println("nihao git");
+        System.out.println("niha二手房东莞o git");
     }
 }
